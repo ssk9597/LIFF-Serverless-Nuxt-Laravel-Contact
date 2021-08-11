c4d011b824491106365a36716da7440a8bbe6114
sasaki <skyo.97.admj@gmail.com>
2021-08-11 15:21:03 +0900

    test

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,30 @@
 <template>
   <div>
     <h2>こんにちは</h2>
+    <FormInput
+      :rules="'required|max:10'"
+      :type="'text'"
+      :placeholder="'姓'"
+      :value="firstName"
+      @input="firstName = $event"
+    />
     <Form />
   </div>
 </template>
 
 <script>
 import Form from '@/components/Template/Form.vue';
+import FormInput from '@/components/Atoms/FormInput.vue';
+
 export default {
   components: {
     Form,
+    FormInput,
+  },
+  data() {
+    return {
+      firstName: '',
+    };
   },
   mounted() {
     window.liff.init({
